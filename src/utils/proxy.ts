32564c1597617dede31cdc9c8272e80774eea3fa
aiman <abduldanmaihaja@gmail.com>
2023-01-07 13:30:10 +0100

    changed hosting plartform

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -1,4 +1,5 @@
 const DOMAIN: { URL: String } = {
-  URL: "https://stark-brook-15908.herokuapp.com",
+  // URL: "https://stark-brook-15908.herokuapp.com",
+  URL: "https://github.com/Abdulaiman/TYPESCRIPT-REACT-employee-management-system",
 };
 export default DOMAIN;
